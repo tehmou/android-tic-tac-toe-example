22693b5c59d9d3464062704ac31cbcb209678446
tehmou <timo.tuominen@gmail.com>
2017-01-31 12:21:16 +0100

    Add GameGridView with hard coded data

---
+++ app/src/main/java/com/tehmou/book/androidtictactoe/GameSymbol.java
@@ -0,0 +1,5 @@
+package com.tehmou.book.androidtictactoe;
+
+public enum GameSymbol {
+    EMPTY, CIRCLE, CROSS
+}
--- app/src/main/java/com/tehmou/book/androidtictactoe/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidtictactoe/MainActivity.java
@@ -9,5 +9,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GameGridView gameGridView = (GameGridView) findViewById(R.id.grid_view);
+
+        gameGridView.setData(
+                new GameSymbol[][] {
+                        new GameSymbol[] {
+                                GameSymbol.CIRCLE, GameSymbol.EMPTY, GameSymbol.EMPTY
+                        },
+                        new GameSymbol[] {
+                                GameSymbol.CIRCLE, GameSymbol.CROSS, GameSymbol.EMPTY
+                        },
+                        new GameSymbol[] {
+                                GameSymbol.CROSS, GameSymbol.EMPTY, GameSymbol.EMPTY
+                        }
+                }
+        );
     }
 }
