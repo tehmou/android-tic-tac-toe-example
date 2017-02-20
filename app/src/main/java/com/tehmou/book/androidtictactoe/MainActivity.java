2d837e2c0c3f583b1f757d9790b9dcffd05cd476
tehmou <timo.tuominen@gmail.com>
2017-02-20 18:28:15 -0300

    Add resolving of touch coordinates

--- app/src/main/java/com/tehmou/book/androidtictactoe/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidtictactoe/MainActivity.java
@@ -2,28 +2,67 @@ package com.tehmou.book.androidtictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import com.jakewharton.rxbinding2.view.RxView;
+
+import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final GameSymbol[][] EMPTY_GRID = new GameSymbol[][]{
+            new GameSymbol[]{
+                    GameSymbol.EMPTY, GameSymbol.EMPTY, GameSymbol.EMPTY
+            },
+            new GameSymbol[]{
+                    GameSymbol.EMPTY, GameSymbol.EMPTY, GameSymbol.EMPTY
+            },
+            new GameSymbol[]{
+                    GameSymbol.EMPTY, GameSymbol.EMPTY, GameSymbol.EMPTY
+            }
+    };
+    private static final int GRID_WIDTH = 3;
+    private static final int GRID_HEIGHT = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GameGridView gameGridView = (GameGridView) findViewById(R.id.grid_view);
+        GameGridView gameGridView =
+                (GameGridView) findViewById(R.id.grid_view);
 
-        gameGridView.setData(
-                new GameSymbol[][] {
-                        new GameSymbol[] {
-                                GameSymbol.CIRCLE, GameSymbol.EMPTY, GameSymbol.EMPTY
-                        },
-                        new GameSymbol[] {
-                                GameSymbol.CIRCLE, GameSymbol.CROSS, GameSymbol.EMPTY
-                        },
-                        new GameSymbol[] {
-                                GameSymbol.CROSS, GameSymbol.EMPTY, GameSymbol.EMPTY
-                        }
-                }
-        );
+        gameGridView.setData(EMPTY_GRID);
+
+        Observable<MotionEvent> userTouchEventObservable =
+                RxView.touches(gameGridView)
+                        .filter(ev -> ev.getAction() == MotionEvent.ACTION_UP);
+
+        Observable<GridPosition> gridPositionEventObservable =
+                userTouchEventObservable
+                        .map(ev ->
+                                getGridPosition(
+                                        ev.getX(), ev.getY(),
+                                        gameGridView.getWidth(), gameGridView.getHeight(),
+                                        GRID_WIDTH, GRID_HEIGHT));
+
+        gridPositionEventObservable
+                .subscribe(gridPosition -> Log.d(TAG, gridPosition.toString()));
+    }
+
+    private static GridPosition getGridPosition(float touchX, float touchY,
+                                                int viewWidthPixels, int viewHeightPixels,
+                                                int gridWidth, int gridHeight) {
+        float rx = touchX /
+                (float)(viewWidthPixels+1);
+        int i = (int)(rx * gridWidth);
+
+        float ry = touchY /
+                (float)(viewHeightPixels+1);
+        int n = (int)(ry * gridHeight);
+
+        return new GridPosition(i, n);
     }
 }
