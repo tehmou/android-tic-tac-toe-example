3b605cbb4e7569d9b07a65faac5a3b216beb359e
tehmou <timo.tuominen@gmail.com>
2017-02-21 15:55:12 -0300

    Add InteractiveGameGridView for handling touches

--- app/src/main/java/com/tehmou/book/androidtictactoe/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidtictactoe/MainActivity.java
@@ -1,15 +1,9 @@
 package com.tehmou.book.androidtictactoe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
+import android.support.v7.app.AppCompatActivity;
 
-import com.jakewharton.rxbinding.view.RxView;
-
-import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.subjects.BehaviorSubject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -21,23 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GameGridView gameGridView =
-                (GameGridView) findViewById(R.id.grid_view);
+        InteractiveGameGridView gameGridView =
+                (InteractiveGameGridView) findViewById(R.id.grid_view);
 
-        Observable<MotionEvent> userTouchEventObservable =
-                RxView.touches(gameGridView)
-                        .filter(ev -> ev.getAction() == MotionEvent.ACTION_UP);
-
-        Observable<GridPosition> gridPositionEventObservable =
-                userTouchEventObservable
-                        .map(ev ->
-                                getGridPosition(
-                                        ev.getX(), ev.getY(),
-                                        gameGridView.getWidth(), gameGridView.getHeight(),
-                                        gameGridView.getGridWidth(),
-                                        gameGridView.getGridHeight()));
-
-        gameViewModel = new GameViewModel(gridPositionEventObservable);
+        gameViewModel = new GameViewModel(
+                gameGridView.getTouchesOnGrid()
+        );
 
         gameViewModel.getGameGrid()
                 .observeOn(AndroidSchedulers.mainThread())
@@ -50,19 +33,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         gameViewModel.unsubscribe();
-    }
-
-    private static GridPosition getGridPosition(float touchX, float touchY,
-                                                int viewWidthPixels, int viewHeightPixels,
-                                                int gridWidth, int gridHeight) {
-        float rx = touchX /
-                (float)(viewWidthPixels+1);
-        int i = (int)(rx * gridWidth);
-
-        float ry = touchY /
-                (float)(viewHeightPixels+1);
-        int n = (int)(ry * gridHeight);
-
-        return new GridPosition(i, n);
     }
 }
