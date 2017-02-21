99b5b24aa5065af85eb6c9c6676869ba3f0fafd1
tehmou <timo.tuominen@gmail.com>
2017-02-21 16:57:11 -0300

    Add GameState for lastPlayedSymbol and changing turns

---
+++ app/src/main/java/com/tehmou/book/androidtictactoe/pojo/GameState.java
@@ -0,0 +1,25 @@
+package com.tehmou.book.androidtictactoe.pojo;
+
+public class GameState {
+    private final GameGrid gameGrid;
+    private final GameSymbol lastPlayedSymbol;
+
+    public GameState(GameGrid gameGrid, GameSymbol lastPlayedSymbol) {
+        this.gameGrid = gameGrid;
+        this.lastPlayedSymbol = lastPlayedSymbol;
+    }
+
+    public GameGrid getGameGrid() {
+        return gameGrid;
+    }
+
+    public GameSymbol getLastPlayedSymbol() {
+        return lastPlayedSymbol;
+    }
+
+    public GameState setSymbolAt(GridPosition gridPosition, GameSymbol symbol) {
+        return new GameState(
+                gameGrid.setSymbolAt(gridPosition, symbol), symbol
+        );
+    }
+}
