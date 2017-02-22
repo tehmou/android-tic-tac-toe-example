8f19754bac68a3b82378ca7184839b2ffe19a97a
tehmou <timo.tuominen@gmail.com>
2017-02-22 20:19:59 -0300

    Increase grid size and update assets

--- app/src/main/java/com/tehmou/book/androidtictactoe/GameViewModel.java
+++ app/src/main/java/com/tehmou/book/androidtictactoe/GameViewModel.java
@@ -13,8 +13,8 @@ import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.subjects.BehaviorSubject;
 
 public class GameViewModel {
-    private static final int GRID_WIDTH = 3;
-    private static final int GRID_HEIGHT = 3;
+    private static final int GRID_WIDTH = 7;
+    private static final int GRID_HEIGHT = 7;
     private static final GameGrid EMPTY_GRID = new GameGrid(GRID_WIDTH, GRID_HEIGHT);
     private static final GameState EMPTY_GAME = new GameState(EMPTY_GRID, GameSymbol.EMPTY);
 
