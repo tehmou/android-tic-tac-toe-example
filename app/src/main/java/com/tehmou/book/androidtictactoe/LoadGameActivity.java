8a4389c4f0c74c987facd9b3f2ccb5141f24bad1
tehmou <timo.tuominen@gmail.com>
2017-02-24 17:20:44 -0300

    Move GameModel to Application

--- app/src/main/java/com/tehmou/book/androidtictactoe/LoadGameActivity.java
+++ app/src/main/java/com/tehmou/book/androidtictactoe/LoadGameActivity.java
@@ -3,7 +3,10 @@ package com.tehmou.book.androidtictactoe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tehmou.book.androidtictactoe.data.GameModel;
+
 public class LoadGameActivity extends AppCompatActivity {
+    private GameModel gameModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,8 @@ public class LoadGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_load_game);
 
         setTitle("Load Previous Game");
+
+        // Get the shared GameModel
+        gameModel = ((GameApplication) getApplication()).getGameModel();
     }
 }
