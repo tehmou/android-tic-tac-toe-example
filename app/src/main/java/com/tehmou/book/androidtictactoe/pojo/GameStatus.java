e20455044a32366890980a1d2b0c6b8ed1c87346
tehmou <timo.tuominen@gmail.com>
2017-03-03 14:41:03 +0100

    Add winning position to GameStatus

--- app/src/main/java/com/tehmou/book/androidtictactoe/pojo/GameStatus.java
+++ app/src/main/java/com/tehmou/book/androidtictactoe/pojo/GameStatus.java
@@ -1,31 +1,41 @@
 package com.tehmou.book.androidtictactoe.pojo;
 
 public class GameStatus {
-    private final boolean isEnded;
     private final GameSymbol winner;
+    private final GridPosition winningPositionStart;
+    private final GridPosition winningPositionEnd;
 
-    protected GameStatus(boolean isEnded) {
-        this(isEnded, null);
-    }
-
-    protected GameStatus(boolean isEnded, GameSymbol winner) {
-        this.isEnded = isEnded;
+    protected GameStatus(GameSymbol winner,
+                         GridPosition winningPositionStart,
+                         GridPosition winningPositionEnd) {
         this.winner = winner;
+        this.winningPositionStart = winningPositionStart;
+        this.winningPositionEnd = winningPositionEnd;
     }
 
     public boolean isEnded() {
-        return isEnded;
+        return winner != null;
     }
 
     public GameSymbol getWinner() {
         return winner;
     }
 
-    public static GameStatus ended(GameSymbol winner) {
-        return new GameStatus(true, winner);
+    public GridPosition getWinningPositionStart() {
+        return winningPositionStart;
+    }
+
+    public GridPosition getWinningPositionEnd() {
+        return winningPositionEnd;
+    }
+
+    public static GameStatus ended(GameSymbol winner,
+                                   GridPosition winningPositionStart,
+                                   GridPosition winningPositionEnd) {
+        return new GameStatus(winner, winningPositionStart, winningPositionEnd);
     }
 
     public static GameStatus ongoing() {
-        return new GameStatus(false);
+        return new GameStatus(null, null, null);
     }
 }
